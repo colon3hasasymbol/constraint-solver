160f101a4ad823b56848e113e84bafac3a5a8b77
Your Name <you@example.com>
2025-11-22 23:11:35 -0500

    added a trail to particle c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <cglm/vec2.h>
 #include <raylib.h>
 #include <elc/core.h>
+#include <stdlib.h>
 
 typedef struct Particle {
     vec2 position;
@@ -17,6 +18,11 @@ typedef struct OriginConstraint {
     Particle* particle;
     float distance;
 } OriginConstraint;
+
+typedef struct ParticleTrail {
+    Vector2* points;
+    u32 n_points, max_points;
+} ParticleTrail;
 
 float originConstraint(OriginConstraint constraint) {
     return fabsf(glm_vec2_norm(constraint.particle->position)) - constraint.distance;
@@ -69,36 +75,68 @@ void applyParticleVelocity(Particle* particle, float dt /* delta time */) {
     glm_vec2_muladds(particle->velocity, dt, particle->position);
 }
 
+ParticleTrail createParticleTrail(u32 max_points) {
+    return (ParticleTrail){.max_points = max_points, .points = malloc(max_points * sizeof(Vector2))};
+}
+
+void resizeParticleTrail(ParticleTrail* trail, u32 max_points) {
+    trail->max_points = max_points;
+    trail->points = realloc(trail->points, max_points * sizeof(Vector2));
+}
+
+void particleTrailAddPoint(ParticleTrail* trail, vec2 point) {
+    if (trail->n_points + 1 > trail->max_points) resizeParticleTrail(trail, (trail->max_points * 1.5f) + 1);
+    trail->points[trail->n_points++] = (Vector2){((float)800 / 2) + (point[0] * 50), ((float)600 / 2) + (point[1] * 50)};
+}
+
+void drawParticleTrail(ParticleTrail trail) {
+    DrawLineStrip(trail.points, trail.n_points, WHITE);
+}
+
 int main() {
     InitWindow(800, 600, "constraint solver");
-    SetTargetFPS(120);
+    SetTargetFPS(240);
 
-    Particle particle_b = {.position = {2.0f}};
     Particle particle_a = {.position = {1.0f}};
+    Particle particle_b = {.position = {2.0f}};
+    Particle particle_c = {.position = {3.0f}};
     OriginConstraint origin_constraint = {.distance = 1.0f, .particle = &particle_a};
-    DistanceConstraint distance_constraint = {.distance = 1.0f, .particle_a = &particle_a, .particle_b = &particle_b};
+    DistanceConstraint distance_constraint_a = {.distance = 1.0f, .particle_a = &particle_a, .particle_b = &particle_b};
+    DistanceConstraint distance_constraint_b = {.distance = 1.0f, .particle_a = &particle_b, .particle_b = &particle_c};
+
+    ParticleTrail trail = createParticleTrail(100);
 
     while (!WindowShouldClose()) {
-        float dt = 1.0f / 120.0f;
+        float dt = 1.0f / 240.0f;
+
+        particleTrailAddPoint(&trail, particle_c.position);
 
         applyParticleGravity(&particle_a, dt);
         applyParticleGravity(&particle_b, dt);
+        applyParticleGravity(&particle_c, dt);
 
-        for (u32 j = 0; j < 10; j++) {
+        for (u32 j = 0; j < 20; j++) {
             applyOriginConstraint(origin_constraint, dt);
-            applyDistanceConstraint(distance_constraint, dt);
+            applyDistanceConstraint(distance_constraint_a, dt);
+            applyDistanceConstraint(distance_constraint_b, dt);
         }
 
         applyParticleVelocity(&particle_a, dt);
         applyParticleVelocity(&particle_b, dt);
+        applyParticleVelocity(&particle_c, dt);
 
         BeginDrawing();
 
         ClearBackground(BLACK);
 
+        drawParticleTrail(trail);
+
         DrawCircle(800 / 2, 600 / 2, 5, YELLOW);
         DrawCircle(((float)800 / 2) + (particle_a.position[0] * 50), ((float)600 / 2) + (particle_a.position[1] * 50), 5, BLUE);
         DrawCircle(((float)800 / 2) + (particle_b.position[0] * 50), ((float)600 / 2) + (particle_b.position[1] * 50), 5, RED);
+        DrawCircle(((float)800 / 2) + (particle_c.position[0] * 50), ((float)600 / 2) + (particle_c.position[1] * 50), 5, ORANGE);
+
+        DrawFPS(10, 10);
 
         EndDrawing();
     }

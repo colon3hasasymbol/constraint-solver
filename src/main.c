c827c4a0d1aa480ffc6b4c71a0255219dfafd27e
Your Name <you@example.com>
2025-11-23 18:59:16 -0500

    added rotation indicators

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <cglm/io.h>
+#include <cglm/util.h>
 #include <cglm/vec2.h>
 #include <raylib.h>
 #include <elc/core.h>
@@ -6,6 +8,7 @@
 typedef struct Particle {
     vec2 position;
     vec2 velocity;
+    float rotation;
 } Particle;
 
 typedef struct DistanceConstraint {
@@ -23,6 +26,12 @@ typedef struct ParticleTrail {
     Vector2* points;
     u32 n_points, max_points;
 } ParticleTrail;
+
+typedef struct PositionSpring {
+    Particle* particle;
+    vec2 position;
+    float distance, stiffness;
+} PositionSpring;
 
 float originConstraint(OriginConstraint constraint) {
     return fabsf(glm_vec2_norm(constraint.particle->position)) - constraint.distance;
@@ -67,6 +76,17 @@ void applyDistanceConstraint(DistanceConstraint constraint, float dt) {
     glm_vec2_muladds(&jacobian[2], distanceViolation(constraint, jacobian, dt) / glm_vec4_dot(jacobian, jacobian), constraint.particle_b->velocity);
 }
 
+float positionSpringStrength(PositionSpring spring) {
+    return (fabsf(glm_vec2_distance(spring.position, spring.particle->position)) - spring.distance) * spring.stiffness;
+}
+
+void applyPositionSpring(PositionSpring spring) {
+    vec2 direction;
+    glm_vec2_sub(spring.position, spring.particle->position, direction);
+    glm_vec2_normalize(direction);
+    glm_vec2_muladds(direction, positionSpringStrength(spring), spring.particle->velocity);
+}
+
 void applyParticleGravity(Particle* particle, float dt /* delta time */) {
     particle->velocity[1] += 9.8f * dt; /* apply gravity * delta time */
 }
@@ -93,6 +113,15 @@ void drawParticleTrail(ParticleTrail trail) {
     DrawLineStrip(trail.points, trail.n_points, WHITE);
 }
 
+void drawParticle(Particle particle, Color color) {
+    Vector2 pos = {((float)800 / 2) + (particle.position[0] * 50), ((float)600 / 2) + (particle.position[1] * 50)};
+    DrawCircleV(pos, 5, color);
+    vec2 rotation_indicator = {3.0f};
+    glm_vec2_rotate(rotation_indicator, particle.rotation, rotation_indicator);
+    glm_vec2_add((float*)&pos, rotation_indicator, (float*)&pos);
+    DrawCircleV(pos, 1.5f, BLACK);
+}
+
 int main() {
     InitWindow(800, 600, "constraint solver");
     SetTargetFPS(240);
@@ -103,8 +132,9 @@ int main() {
     OriginConstraint origin_constraint = {.distance = 1.0f, .particle = &particle_a};
     DistanceConstraint distance_constraint_a = {.distance = 1.0f, .particle_a = &particle_a, .particle_b = &particle_b};
     DistanceConstraint distance_constraint_b = {.distance = 1.0f, .particle_a = &particle_b, .particle_b = &particle_c};
+    PositionSpring spring = {.stiffness = 1.0f};
 
-    ParticleTrail trail = createParticleTrail(100);
+    ParticleTrail trail = createParticleTrail(ELC_KILOBYTE);
 
     while (!WindowShouldClose()) {
         float dt = 1.0f / 240.0f;
@@ -114,6 +144,16 @@ int main() {
         applyParticleGravity(&particle_a, dt);
         applyParticleGravity(&particle_b, dt);
         applyParticleGravity(&particle_c, dt);
+
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+            Vector2 mouse = GetMousePosition();
+            glm_vec2_copy((float*)&mouse, spring.position);
+            glm_vec2_sub(spring.position, (vec2){800.0f / 2, 600.0f / 2}, spring.position);
+            glm_vec2_divs(spring.position, 50.0f, spring.position);
+            spring.particle = &particle_c;
+            glm_vec2_scale(spring.particle->velocity, 0.75f, spring.particle->velocity);
+            applyPositionSpring(spring);
+        }
 
         for (u32 j = 0; j < 20; j++) {
             applyOriginConstraint(origin_constraint, dt);
@@ -132,9 +172,9 @@ int main() {
         drawParticleTrail(trail);
 
         DrawCircle(800 / 2, 600 / 2, 5, YELLOW);
-        DrawCircle(((float)800 / 2) + (particle_a.position[0] * 50), ((float)600 / 2) + (particle_a.position[1] * 50), 5, BLUE);
-        DrawCircle(((float)800 / 2) + (particle_b.position[0] * 50), ((float)600 / 2) + (particle_b.position[1] * 50), 5, RED);
-        DrawCircle(((float)800 / 2) + (particle_c.position[0] * 50), ((float)600 / 2) + (particle_c.position[1] * 50), 5, ORANGE);
+        drawParticle(particle_a, BLUE);
+        drawParticle(particle_b, RED);
+        drawParticle(particle_c, ORANGE);
 
         DrawFPS(10, 10);
 
